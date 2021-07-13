7b96242fbb4f35a4fbc5aac045b781855bea7ffa
liangxiaojun <fatalxiao@163.com>
2021-07-13 14:12:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -15,16 +15,27 @@ import createFailureResponseMiddleware from './middlewares/FailureResponseMiddle
 export ApiStatus from './statics/ApiStatus';
 
 /**
+ * Default vivy-api options
+ * @type {{checkResponseStatus: (function(*)), modelNameSpace: string}}
+ */
+const DEFAULT_OPTIONS = {
+    modelNameSpace: 'apiStatus',
+    checkResponseStatus: response => response.status >= 200 && response.status < 300
+};
+
+/**
  * Create Vivy api plugin
  * @param options
  * @returns {{}}
  */
 export default function createVivyApiPlugin(options = {}) {
 
+    const op = {...DEFAULT_OPTIONS, ...options};
+
     const {
         modelNameSpace,
         checkResponseStatus, successResponseHandler, failureResponseHandler
-    } = options;
+    } = op;
 
     // Create ModelApiActionMiddleware
     const ModelApiActionMiddleware = createModelApiActionMiddleware();
@@ -32,9 +43,9 @@ export default function createVivyApiPlugin(options = {}) {
     return {
         extraMiddlewares: [
             ModelApiActionMiddleware,
-            createRequestMiddleware(modelNameSpace, options?.checkResponseStatus),
-            createSuccessResponseMiddleware(options?.successResponseHandler),
-            createFailureResponseMiddleware(options?.failureResponseHandler)
+            createRequestMiddleware(modelNameSpace, checkResponseStatus),
+            createSuccessResponseMiddleware(successResponseHandler),
+            createFailureResponseMiddleware(failureResponseHandler)
         ],
         extraModels: [
             createApiStatus(modelNameSpace)

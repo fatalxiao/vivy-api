44989633bc1c6b4d6d4b98de9b087f10f1a47f22
liangxiaojun <fatalxiao@163.com>
2021-07-13 14:14:38 +0800

    update

--- examples/requestApiWithApiPlugin/src/index.js
+++ examples/requestApiWithApiPlugin/src/index.js
@@ -9,7 +9,7 @@ import {Provider} from 'react-redux';
 
 // Import Vivy
 import Vivy, {registerModel} from 'vivy';
-import VivyApi from 'vivy-api';
+import VivyApi from '../../../src';
 
 // Sync component and model
 import UserList from './modules/UserList/containers/UserList';
@@ -25,7 +25,7 @@ const vivy = Vivy(history);
 vivy.use(VivyApi({
 
     // Customized api status model name space ( default is "apiStatus" )
-    modelNameSpace: 'customizedApiStatus',
+    // modelNameSpace: 'customizedApiStatus',
 
     // Customized check response status callback
     // to tell whether response is successful

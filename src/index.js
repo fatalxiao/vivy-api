c3104455276b1538e14d7cf27ff977994f2986d3
liangxiaojun <fatalxiao@163.com>
2022-02-14 15:15:26 +0800

    update vivy-api

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ const DEFAULT_OPTIONS = {
 
 /**
  * Create Vivy api plugin
- * @param options {Object}
- * @returns {{onRegisterModel: onRegisterModel, extraModels: ({}|*)[], extraMiddlewares: ((function({dispatch?: *, getState?: *}): function(*): function(*=): *)|*|(function({dispatch: *}): function(*): function(*=): Promise<*|undefined>)|(function({dispatch: *, getState: *}): function(*=): function(*=): *))[]}}
+ * @param options
+ * @returns {{onRegisterModel: onRegisterModel, extraModels: ({reducers: {request: function(Object, {nameSpace: string, apiActionName: string}): {}, success: function(Object, {nameSpace: string, apiActionName: string}): {}, failure: function(Object, {nameSpace: string, apiActionName: string}): {}}, nameSpace, state: {}}|*)[], extraMiddlewares: ((function({dispatch: *}): function(*): function(*=): Promise<*|undefined>)|*|(function({dispatch: *, getState: *}): function(*=): function(*=): *))[]}}
  */
 export default function VivyApi(options = {}) {
 
